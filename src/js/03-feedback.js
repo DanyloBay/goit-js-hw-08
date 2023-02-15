b85b124946737d5542cb32cad2440b902046ccf1
Danylo Bay <danylobay@gmail.com>
2023-02-15 19:14:21 +0100

    task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  formEl: document.querySelector('form'),
+  emailEl: document.querySelector('input'),
+  nameEl: document.querySelector('textarea'),
+};
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+refs.formEl.addEventListener(
+  'input',
+  throttle(() => {
+    const storageValue = { email: refs.emailEl.value, name: refs.nameEl.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storageValue));
+  }, 500)
+);
+
+refs.formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  refs.formEl.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+});
+
+const load = key => {
+  try {
+    const storageState = localStorage.getItem(key);
+    return storageState === null ? undefined : JSON.parse(storageState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const storageFields = load(LOCALSTORAGE_KEY);
+if (storageFields) {
+  refs.emailEl.value = storageFields.email;
+  refs.nameEl.value = storageFields.name;
+}
