efbbc34c58aac3e3d27cd1f41252154cf8b54001
Danylo Bay <danylobay@gmail.com>
2023-02-15 17:56:47 +0100

    task_2
    saved start video (throttle)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'play',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', `${seconds}`);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (second) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
